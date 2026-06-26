982288fb30e31967999e1fe8a2e78e630e18c7bb
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2026-06-26 18:48:01 -0400

    Update RcloneRcd.java

--- app/src/main/java/ca/pkay/rcloneexplorer/RcloneRcd.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/RcloneRcd.java
@@ -193,7 +193,7 @@ public class RcloneRcd {
 
         // ignore chtimes errors
         // ref: https://github.com/rclone/rclone/issues/2446
-        environmentValues.add("RCLONE_LOCAL_NO_SET_MODTIME=true");
+        // environmentValues.add("RCLONE_LOCAL_NO_SET_MODTIME=true");
 
         // The pre-built linux rclone binaries do not know how to find the Android certificate store.
         // We set SSL_CERT_DIR to Android's native certificate store path.
